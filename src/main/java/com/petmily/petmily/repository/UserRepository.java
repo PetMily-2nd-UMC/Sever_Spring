022cec6aa8f403200c5514e7ff86148a02d67dde
ryr0121 <ryr0121@gmail.com>
2022-07-24 14:43:03 +0900

    POST function of Communication Post, Comment, Like (need to fix retrieve/update/delete)

--- src/main/java/com/petmily/petmily/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.petmily.petmily.repository;
-
-import com.petmily.petmily.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
